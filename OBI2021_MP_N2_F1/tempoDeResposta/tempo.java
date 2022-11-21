ec0d83768bcfd85b257ccbca2241542718b6fac5
nicoIwas <nickuhr@usp.br>
2022-11-21 11:27:54 -0300

    adicionando o código fonte o executável da implementação provisória sem a solução do problema de tempo de resposta (que agora será solucionada em java

---
+++ OBI2021_MP_N2_F1/tempoDeResposta/tempo.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class tempo{
+
+    String nome;
+    int tempoResp;
+
+    public tempo(String nome){
+        this.nome = nome;
+        this.tempoResp = 0;
+    }
+
+
+public static void main(String[] args) {
+    
+    
+    for (int i = 0; i < ; i++) {
+        
+        
+        
+
+        if(modelo.toCharArray()[0] == 'R'){
+        
+           
+        }
+
+        else if(modelo.toCharArray()[0] == 'T'){
+
+        }   
+
+        else if(modelo.toCharArray()[0]=='E'){
+                 
+            }
+        }
+
+        for (int j = 0; j < array.length; j++) {
+
+        }   
+    } 
+}
+
+
